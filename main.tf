560b42ee35ad4b7c5f248b3228e407311a1eed07
gauravgupta745 <gaurav.guptagn4@gmail.com>
2026-07-01 00:16:27 +0530

    2th commit

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "pinku" {
   name     = "rg-pinku"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "minku" {
+  name     = "rg-minku"
+  location = "West Europe"
+}
